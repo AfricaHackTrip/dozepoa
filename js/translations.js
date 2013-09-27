d6924415bb2484e02c73f38b396ca2e3ef5ba4f4
Michael Bumann <michael@railslove.com>
2013-09-27 14:30:22 +0300

    adding phrases

---
+++ js/translations.js
@@ -0,0 +1,13 @@
+[
+  {
+    icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings', phrases: [
+      {swahili: "Mambo", english: "How are you?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "maybe the most common greeting to start a great conversation"},
+      {swahili: "Sasa", english: "Hi", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "the most common greeting if you do not want to start a conversation"},
+      {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
+      {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
+    ]
+  },
+  {
+  
+  }
+]
