2c24ff03e39838360b9b1783bf894628a911aaa6
Sebastian Kippe <sebastian@kip.pe>
2013-09-27 14:40:45 +0300

    Add jQuery Mobile, grid and wordlist templates

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 var categoriesTemplate = Handlebars.compile($('#nav-template').html());
+var wordlistTemplate = Handlebars.compile($('#wordlist-template').html());
 
 var navHtml = categoriesTemplate({categories: [
-  {icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings'},
-  {icon: 'food.png', title: 'Dish', english_title: 'Food and Drinks'},
-  {icon: 'directions.png', title: 'Kusaka', english_title: 'Directions'},
-  {icon: 'shopping.png', title: 'Ku-shop', english_title: 'Shopping'}
+  {icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings', gridPosition: "a"},
+  {icon: 'food.png', title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b"},
+  {icon: 'directions.png', title: 'Kusaka', english_title: 'Directions', gridPosition: "a"},
+  {icon: 'shopping.png', title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b"}
 ]});
 
-$('#categories').html(navHtml);
+var wordlistHtml = wordlistTemplate({words: [
+  {title: 'Salamz', english_title: 'Greetings'},
+  {title: 'Dish', english_title: 'Food and Drinks'},
+  {title: 'Kusaka', english_title: 'Directions'},
+  {title: 'Ku-shop', english_title: 'Shopping'}
+]});
+
+// $('#categories').html(navHtml);
+$('#categories').html(wordlistHtml);
