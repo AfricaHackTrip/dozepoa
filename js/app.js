0362e314675558bd5f8ecc3a2cdcfa867c796edc
Sebastian Kippe <sebastian@kip.pe>
2013-09-27 15:30:53 +0300

    Render phrases for categories

--- js/app.js
+++ js/app.js
@@ -2,36 +2,54 @@ var categoriesTemplate = Handlebars.compile($('#nav-template').html());
 var wordlistTemplate = Handlebars.compile($('#wordlist-template').html());
 
 var content = {
-  categories: {
-    salamz: { title: 'Salamz', english_title: 'Greetings', gridPosition: "a", phrases: [
+  categories: [
+    {name: "salamz", title: 'Salamz', english_title: 'Greetings', gridPosition: "a", phrases: [
       {swahili: "Mambo", english: "How are you?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "maybe the most common greeting to start a great conversation"},
       {swahili: "Sasa", english: "Hi", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "the most common greeting if you do not want to start a conversation"},
       {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
       {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
     ]},
-    food: {title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b", phrases: [
+    {name: "food", title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b", phrases: [
       {swahili: "Nini iko?", english: "What's on the menu?", answer: [], explanation: ""},
       {swahili: "Mko na nini leo?", english: "What's on the special of today?", answer: [], explanation: ""},
       {swahili: "Kuna beer", english: "Can I have a beer", answer: [], explanation: ""},
       {swahili: "Kuna soda", english: "Is there soda?", answer: [], explanation: ""}
     ]},
-    directions: {title: 'Kusaka', english_title: 'Directions', gridPosition: "a", phrases: [
+    {name: "directions", title: 'Kusaka', english_title: 'Directions', gridPosition: "a", phrases: [
       {swahili: "Naeza fika vipi [your place]?", english: "How do I get to [your place]?", answer: [], explanation: ""},
       {swahili: "[place] iko wapi", english: "Where is [place]?", answer: [], explanation: ""}
     ]},
-    shopping: {title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b", phrases: [
+    {name: "shopping", title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b", phrases: [
       {swahili: "[fish] ni how much?", english: "how much is the [fish]?", answer: [], explanation: "yes, it is really kind english"},
       {swahili: "[fish] ina uzwa ngapi?", english: "how much is the [fish]?", answer: [], explanation: ""},
       {swahili: "[fish] ni expe sana", english: "That [fish] is way too expensive?", answer: [], explanation: ""}
     ]}
-  }
+  ]
 };
 
 var navHtml = categoriesTemplate(content);
 
 var wordlistHtml = wordlistTemplate({
-  phrases: content.categories["food"]["phrases"]
+  phrases: content.categories[0]["phrases"]
 });
+
+var wordlistHtmlForCategory = function(categoryName) {
+  var category = $.grep( content.categories, function( category, index ) {
+    return category.name === categoryName;
+  })[0];
+
+  return wordlistTemplate({
+    phrases: category.phrases
+  });
+};
 
 $('body').append(navHtml);
 $('body').append(wordlistHtml);
+
+$('#homepage a').on("click", function(){
+  var categoryName = $(this).data('category');
+  console.log(categoryName);
+
+  $('#wordlist').remove();
+  $('body').append(wordlistHtmlForCategory(categoryName));
+});
