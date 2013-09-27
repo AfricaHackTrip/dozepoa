f8ff3daecf524b622367245fafc28385c892529b
Michael Bumann <michael@railslove.com>
2013-09-27 14:45:42 +0300

    more translations

--- js/translations.js
+++ js/translations.js
@@ -8,6 +8,23 @@
     ]
   },
   {
-  
-  }
+    icon: 'food.png', title: 'Dish', english_title: 'Food and Drinks' phrases: [
+      {swahili: "Nini iko?", english: "What's on the menu?", answer: [], explanation: ""},
+      {swahili: "Mko na nini leo?", english: "What's on the special of today?", answer: [], explanation: ""},
+      {swahili: "Kuna beer", english: "Can I have a beer", answer: [], explanation: ""}
+      {swahili: "Kuna soda", english: "Is there soda?", answer: [], explanation: ""}
+    ]
+  },
+  {
+    icon: 'directions.png', title: 'Kusaka', english_title: 'Directions', phrases: [
+      {swahili: "Naeza fika vipi [your place]?", english: "How do I get to [your place]?", answer: [], explanation: ""},
+      {swahili: "[place] iko wapi", english: "Where is [place]?", answer: [], explanation: ""}
+    ]
+  },
+  {
+    icon: 'directions.png', title: 'Kusaka', english_title: 'Directions', phrases: [
+      {swahili: "Naeza fika vipi [your place]?", english: "How do I get to [your place]?", answer: [], explanation: ""},
+      {swahili: "[place] iko wapi", english: "Where is [place]?", answer: ["kushoto - left", "kulia - right", "tembea tu - keep on walking", "rudi - go back"], explanation: ""}
+    ]
+  },
 ]
