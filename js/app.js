50dcb6a1863599181973e7dd61a6f3c4a47c64ce
Sebastian Kippe <sebastian@kip.pe>
2013-09-27 15:00:10 +0300

    Integrate new phrases

--- js/app.js
+++ js/app.js
@@ -9,10 +9,21 @@ var content = {
       {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
       {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
     ]},
-
-    food: {title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b"},
-    directions: {title: 'Kusaka', english_title: 'Directions', gridPosition: "a"},
-    shopping: {title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b"}
+    food: {title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b", phrases: [
+      {swahili: "Nini iko?", english: "What's on the menu?", answer: [], explanation: ""},
+      {swahili: "Mko na nini leo?", english: "What's on the special of today?", answer: [], explanation: ""},
+      {swahili: "Kuna beer", english: "Can I have a beer", answer: [], explanation: ""},
+      {swahili: "Kuna soda", english: "Is there soda?", answer: [], explanation: ""}
+    ]},
+    directions: {title: 'Kusaka', english_title: 'Directions', gridPosition: "a", phrases: [
+      {swahili: "Naeza fika vipi [your place]?", english: "How do I get to [your place]?", answer: [], explanation: ""},
+      {swahili: "[place] iko wapi", english: "Where is [place]?", answer: [], explanation: ""}
+    ]},
+    shopping: {title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b", phrases: [
+      {swahili: "[fish] ni how much?", english: "how much is the [fish]?", answer: [], explanation: "yes, it is really kind english"},
+      {swahili: "[fish] ina uzwa ngapi?", english: "how much is the [fish]?", answer: [], explanation: ""},
+      {swahili: "[fish] ni expe sana", english: "That [fish] is way too expensive?", answer: [], explanation: ""}
+    ]}
   }
 };
 
