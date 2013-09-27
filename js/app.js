646ca437a148bac6f4c17ba659018f818d1fbacf
Sebastian Kippe <sebastian@kip.pe>
2013-09-27 14:55:23 +0300

    Restructure data, add phrases

--- js/app.js
+++ js/app.js
@@ -1,19 +1,26 @@
 var categoriesTemplate = Handlebars.compile($('#nav-template').html());
 var wordlistTemplate = Handlebars.compile($('#wordlist-template').html());
 
-var navHtml = categoriesTemplate({categories: [
-  {icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings', gridPosition: "a"},
-  {icon: 'food.png', title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b"},
-  {icon: 'directions.png', title: 'Kusaka', english_title: 'Directions', gridPosition: "a"},
-  {icon: 'shopping.png', title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b"}
-]});
+var content = {
+  categories: {
+    salamz: { title: 'Salamz', english_title: 'Greetings', gridPosition: "a", phrases: [
+      {swahili: "Mambo", english: "How are you?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "maybe the most common greeting to start a great conversation"},
+      {swahili: "Sasa", english: "Hi", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "the most common greeting if you do not want to start a conversation"},
+      {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
+      {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
+    ]},
 
-var wordlistHtml = wordlistTemplate({words: [
-  {title: 'Salamz', english_title: 'Greetings'},
-  {title: 'Dish', english_title: 'Food and Drinks'},
-  {title: 'Kusaka', english_title: 'Directions'},
-  {title: 'Ku-shop', english_title: 'Shopping'}
-]});
+    food: {title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b"},
+    directions: {title: 'Kusaka', english_title: 'Directions', gridPosition: "a"},
+    shopping: {title: 'Ku-shop', english_title: 'Shopping', gridPosition: "b"}
+  }
+};
+
+var navHtml = categoriesTemplate(content);
+
+var wordlistHtml = wordlistTemplate({
+  phrases: content.categories["salamz"]["phrases"]
+});
 
 // $('#categories').html(navHtml);
 $('#categories').html(wordlistHtml);
