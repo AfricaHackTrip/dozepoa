9085ea53e0c5453f914cd44a37d5c1414bc0eaa9
myamy <vicylant@gmx.net>
2013-09-27 17:51:27 +0300

    niaje vs. niache

--- js/translations.js
+++ js/translations.js
@@ -3,7 +3,7 @@
     icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings', phrases: [
       {swahili: "Mambo", english: "How are you?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "maybe the most common greeting to start a great conversation"},
       {swahili: "Sasa", english: "Hi", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "the most common greeting if you do not want to start a conversation"},
-      {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
+      {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else - do NOT mix up with niache"},
       {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
     ]
   },
