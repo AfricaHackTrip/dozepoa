735ee1a034095beea750a54b1b72c3f958bf27c5
Alexander Lang <alex@upstre.am>
2013-09-27 14:11:16 +0300

    adds more categories

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var categoriesTemplate = Handlebars.compile($('#nav-template').html());
 
 var navHtml = categoriesTemplate({categories: [
-  {icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings'}
+  {icon: 'salamz.png', title: 'Salamz', english_title: 'Greetings'},
+  {icon: 'food.png', title: 'Dish', english_title: 'Food and Drinks'},
+  {icon: 'directions.png', title: 'Kusaka', english_title: 'Directions'},
+  {icon: 'shopping.png', title: 'Ku-shop', english_title: 'Shopping'}
 ]});
 
 $('#categories').html(navHtml);
