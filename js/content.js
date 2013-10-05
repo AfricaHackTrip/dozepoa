4da69f2ac2e580b6bf2178818af765580c4cf8b2
Sebastian Kippe <sebastian@kip.pe>
2013-10-05 12:38:23 +0300

    Add Luganda and Lugbara

--- js/content.js
+++ js/content.js
@@ -5,24 +5,32 @@ Dozepoa.content = {
     {name: "salamz", title: 'Salamz', english_title: 'Greetings', gridPosition: "a", phrases: [
       {
         swahili: "Mambo",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "How are you?",
         answer: ["Poa - Cool", "Niko poa - I'm fine"],
         explanation: "maybe the most common greeting to start a great conversation"
       },
       {
         swahili: "Sasa",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Hi",
         answer: ["Poa - Cool", "Niko poa - I'm fine"],
         explanation: "the most common greeting if you do not want to start a conversation"
       },
       {
         swahili: "Niaje",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "How is it?",
         answer: ["Poa - Cool", "Niko poa - I'm fine"],
         explanation: "checking on someone else"
       },
       {
         swahili: "Sasa niaje",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Hey, how is it now?",
         answer: ["Poa - Cool", "Niko poa - I'm fine"],
         explanation: ""
@@ -31,24 +39,32 @@ Dozepoa.content = {
     {name: "food", title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b", phrases: [
       {
         swahili: "Nini iko?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "What's on the menu?",
         answer: [],
         explanation: ""
       },
       {
         swahili: "Mko na nini leo?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "What's on the special of today?",
         answer: [],
         explanation: ""
       },
       {
         swahili: "Kuna beer",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Can I have a beer",
         answer: [],
         explanation: ""
       },
       {
         swahili: "Kuna soda",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Is there soda?",
         answer: [],
         explanation: ""
@@ -57,12 +73,16 @@ Dozepoa.content = {
     {name: "directions", title: 'Kusaka', english_title: 'Directions', gridPosition: "a", phrases: [
       {
         swahili: "Naeza fika vipi [your place]?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "How do I get to [your place]?",
         answer: [],
         explanation: ""
       },
       {
         swahili: "[place] iko wapi",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Where is [place]?",
         answer: [],
         explanation: ""
@@ -71,24 +91,32 @@ Dozepoa.content = {
     {name: "whatthe", title: 'Ei...?!', english_title: 'What the..?!', gridPosition: "b", phrases: [
       {
         swahili: "Ei...",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "What the...?",
         answer: [],
         explanation: ""
       },
       {
         swahili: "Eishhhh",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "ouch!",
         answer: [],
         explanation: ""
       },
       {
         swahili: "wa wa wa",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "expresion of being surprised",
         answer: [],
         explanation: ""
       },
       {
         swahili: "Kwenda uko",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "get outta here",
         answer: [],
         explanation: ""
@@ -97,18 +125,24 @@ Dozepoa.content = {
     {name: "shopping", title: 'Ku-shop', english_title: 'Shopping', gridPosition: "a", phrases: [
       {
         swahili: "[fish] ni how much?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "how much is the [fish]?",
         answer: [],
         explanation: "yes, it is really kind english"
       },
       {
         swahili: "[fish] ina uzwa ngapi?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "how much is the [fish]?",
         answer: [],
         explanation: ""
       },
       {
         swahili: "[fish] ni expe sana",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "That [fish] is way too expensive?",
         answer: [],
         explanation: ""
@@ -117,18 +151,24 @@ Dozepoa.content = {
     {name: "pickup", title: 'Mambo m-super', english_title: 'Pickup lines',gridPosition: "b", phrases: [
       {
         swahili: "Mambo m-super",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Hey gorgeous",
         answer: [],
         explanation: "Only when adressing girls!"
       },
       {
         swahili: "Mambo m-handsome",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "Hey handsome",
         answer: [],
         explanation: "Only when adressing guys!"
       },
       {
         swahili: "We ni kuitaje?",
+        luganda: "Oli otya",
+        lugbara: "Mi ngoni",
         english: "How do I call you?",
         answer: [],
         explanation: "to any gender"
