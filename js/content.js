3628c7ed132a1c1bb190834f07b3c6f2512f0088
Sebastian Kippe <sebastian@kip.pe>
2013-10-05 12:19:09 +0300

    Move content object to separate file

---
+++ js/content.js
@@ -0,0 +1,36 @@
+Dozepoa.content = {
+  categories: [
+    {name: "salamz", title: 'Salamz', english_title: 'Greetings', gridPosition: "a", phrases: [
+      {swahili: "Mambo", english: "How are you?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "maybe the most common greeting to start a great conversation"},
+      {swahili: "Sasa", english: "Hi", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "the most common greeting if you do not want to start a conversation"},
+      {swahili: "Niaje", english: "How is it?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: "checking on someone else"},
+      {swahili: "Sasa niaje", english: "Hey, how is it now?", answer: ["Poa - Cool", "Niko poa - I'm fine"], explanation: ""}
+    ]},
+    {name: "food", title: 'Dish', english_title: 'Food and Drinks', gridPosition: "b", phrases: [
+      {swahili: "Nini iko?", english: "What's on the menu?", answer: [], explanation: ""},
+      {swahili: "Mko na nini leo?", english: "What's on the special of today?", answer: [], explanation: ""},
+      {swahili: "Kuna beer", english: "Can I have a beer", answer: [], explanation: ""},
+      {swahili: "Kuna soda", english: "Is there soda?", answer: [], explanation: ""}
+    ]},
+    {name: "directions", title: 'Kusaka', english_title: 'Directions', gridPosition: "a", phrases: [
+      {swahili: "Naeza fika vipi [your place]?", english: "How do I get to [your place]?", answer: [], explanation: ""},
+      {swahili: "[place] iko wapi", english: "Where is [place]?", answer: [], explanation: ""}
+    ]},
+    {name: "whatthe", title: 'Ei...?!', english_title: 'What the..?!', gridPosition: "b", phrases: [
+      {swahili: "Ei...", english: "What the...?", answer: [], explanation: ""},
+      {swahili: "Eishhhh", english: "ouch!", answer: [], explanation: ""},
+      {swahili: "wa wa wa", english: "expresion of being surprised", answer: [], explanation: ""},
+      {swahili: "Kwenda uko",english: "get outta here", answer: [], explanation: ""}
+    ]},
+    {name: "shopping", title: 'Ku-shop', english_title: 'Shopping', gridPosition: "a", phrases: [
+      {swahili: "[fish] ni how much?", english: "how much is the [fish]?", answer: [], explanation: "yes, it is really kind english"},
+      {swahili: "[fish] ina uzwa ngapi?", english: "how much is the [fish]?", answer: [], explanation: ""},
+      {swahili: "[fish] ni expe sana", english: "That [fish] is way too expensive?", answer: [], explanation: ""}
+    ]},
+    {name: "pickup", title: 'Mambo m-super', english_title: 'Pickup lines',gridPosition: "b", phrases: [
+      {swahili: "Mambo m-super", english: "Hey gorgeous", answer: [], explanation: "Only when adressing girls!"},
+      {swahili: "Mambo m-handsome", english: "Hey handsome", answer: [], explanation: "Only when adressing guys!"},
+      {swahili: "We ni kuitaje?", english: "How do I call you?", answer: [], explanation: "to any gender"},
+    ]}
+  ]
+};
